e637bfe427d805c1477895cbfb88847269c3902f
FRickReich <frickreich@gmail.com>
2022-01-04 09:11:44 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Slider } from './Components';
 
+import './App.scss';
+
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+import img4 from './images/img4.jpg';
+import img5 from './images/img5.jpg';
+
+const imgArray = [ img1, img2, img3, img4, img5 ];
+// const imgArray = [];
 
 function App() {
-  return (
-    <div className="App">
-        <p>Happy Hacking!</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Slider images={ imgArray } />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/index.js
@@ -0,0 +1,9 @@
+import { Slider } from './Slider/Slider';
+import { Slide } from './Slide/Slide';
+import { Error } from './Error/Error';
+
+export {
+    Slider,
+    Slide,
+    Error
+};
